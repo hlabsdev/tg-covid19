8bf094b1b69b2be6ed8662e179fce5d75e1b7f35
H-Labs99 <danielgolo82@gmail.com>
2020-03-24 21:38:28 +0100

    Ajout de nouveaux element

--- lib/utils/Alertes.dart
+++ lib/utils/Alertes.dart
@@ -7,24 +7,61 @@ class Alertes extends StatefulWidget {
 }
 
 class _AlertesState extends State<Alertes> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
   var initialisationSettingsAndroid;
   var initialisationSettingsIOS;
   var initialisationSettings;
-  void showAlerte(){
 
-  }
+  void showAlerte() {}
 
   @override
   void initState() {
     super.initState();
-    initialisationSettingsAndroid = new AndroidInitializationSettings('app_icon');
-    initialisationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+    initialisationSettingsAndroid =
+        new AndroidInitializationSettings('app_icon');
+    initialisationSettingsIOS = new IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
   }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    await showDialog<void>(
+        context: context,
+//      barrierDismissible: barrierDismissible,
+        // false = user must tap button, true = tap outside dialog
+        builder: (BuildContext dialogContext) => CupertinoAlertDialog(
+              title: Text("Titre"),
+              content: Text("Contenu"),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text("Ok"),
+                  onPressed: () async{
+                    Navigator.of(context, rootNavigator: true).pop(
+                      await Navigator.push(context,
+                        MaterialPageRoute(builder: (context)=>SecondRoute())
+                      )
+                    );
+                  },
+                )
+              ],
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();  }
+}
+class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Alertepage'),
+      ),
+      
     );
   }
 }
+
